53ac7cd09ae348d82d0a47fbfed5facf1dc93ac4
dhlee777 <ukki5782@gmail.com>
2024-10-09 00:42:02 +0900

    chore: server initial commit

---
+++ server/wapor/src/main/java/com/wap/wapor/WaporApplication.java
@@ -0,0 +1,13 @@
+package com.wap.wapor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaporApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaporApplication.class, args);
+	}
+
+}
---
+++ server/wapor/src/test/java/com/wap/wapor/WaporApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wap.wapor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaporApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
