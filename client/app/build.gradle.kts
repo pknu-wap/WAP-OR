2fd1dea6ce68a574302716b68b9827868df9e509
mututu17 <chil_s_ng@naver.com>
2024-10-15 00:56:26 +0900

    Feat: 하단바 추가
    반응형 X, 이렇게 하는게 맞나 싶다

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
