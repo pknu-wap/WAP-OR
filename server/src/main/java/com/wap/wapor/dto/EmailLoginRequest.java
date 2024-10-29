d5b1dd53bf7d04a53d2588cbf1be5ce258f2a51d
pu2rile <3o920@naver.com>
2024-10-29 13:19:58 +0900

    refactor: 코드 간소화를 위해 Lombok 적용

--- server/src/main/java/com/wap/wapor/dto/EmailLoginRequest.java
+++ server/src/main/java/com/wap/wapor/dto/EmailLoginRequest.java
@@ -1,27 +1,11 @@
 package com.wap.wapor.dto;
-import com.wap.wapor.domain.UserType;
 
+import com.wap.wapor.domain.UserType;
+import lombok.Data;
+
+@Data
 public class EmailLoginRequest {
     private UserType userType;
     private String password;
     private String identifier;
-
-    public UserType getUserType() {
-        return userType;
-    }
-    public void setUserType(UserType userType) {
-        this.userType = userType;
-    }
-    public String getIdentifier() {
-        return identifier;
-    }
-    public void setIdentifier(String identifier) {
-        this.identifier = identifier;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
