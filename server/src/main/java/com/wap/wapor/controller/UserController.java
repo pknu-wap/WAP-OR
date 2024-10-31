6445f02ed693cb304246f60615734c4811b7eb43
pu2rile <3o920@naver.com>
2024-11-01 01:58:09 +0900

    feature: UserController에 JSON 형식 응답 로직 추가

--- server/src/main/java/com/wap/wapor/controller/UserController.java
+++ server/src/main/java/com/wap/wapor/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.wap.wapor.controller;
 import com.wap.wapor.dto.EmailLoginRequest;
+import com.wap.wapor.repository.UserRepository;
 import com.wap.wapor.service.UserService;
-import com.wap.wapor.domain.UserType;
+import com.wap.wapor.dto.LoginResponse;
+import com.wap.wapor.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     /*
@@ -28,10 +32,19 @@ public class UserController {
      */
 
     @PostMapping("/login/email")
-    public ResponseEntity<?> emailLogin(@RequestBody EmailLoginRequest emailLoginRequest) {
-        if (emailLoginRequest.getUserType() != UserType.EMAIL) {
-            return ResponseEntity.badRequest().body("Invalid user type for Email login");
+    public ResponseEntity<LoginResponse> emailLogin(@RequestBody EmailLoginRequest emailLoginRequest) {
+        // identifier 값으로 사용자를 검색
+        User user = userRepository.findByIdentifier(emailLoginRequest.getIdentifier()).orElse(null);
+
+        // 사용자가 존재하고 비밀번호가 일치하는지 확인
+        if (user != null && user.getPassword().equals(emailLoginRequest.getPassword())) {
+            // 로그인 성공 시 JSON 응답
+            LoginResponse response = new LoginResponse("success", "Email login successful");
+            return ResponseEntity.ok(response);
+        } else {
+            // 인증 실패 시 JSON 응답
+            LoginResponse errorResponse = new LoginResponse("fail", "Invalid email or password");
+            return ResponseEntity.status(401).body(errorResponse);
         }
-        return userService.emailLogin(emailLoginRequest);
     }
 }
