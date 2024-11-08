c8e409b1858577a8236753d2f509ac8d9903b15f
pu2rile <3o920@naver.com>
2024-11-08 16:27:50 +0900

    feature: 이메일 인증 검증 DTO 추가

---
+++ server/src/main/java/com/wap/wapor/dto/EmailCheck.java
@@ -0,0 +1,15 @@
+package com.wap.wapor.dto;
+
+import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+@Data
+public class EmailCheck {
+    @Email
+    @NotEmpty(message = "이메일을 입력해 주세요")
+    private String email;
+
+    @NotEmpty(message = "인증 번호를 입력해 주세요")
+    private String authCode;
+}
