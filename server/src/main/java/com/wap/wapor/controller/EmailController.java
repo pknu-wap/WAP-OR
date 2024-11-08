17edb118e71755e23751bd02cfd011c9880d0d1f
pu2rile <3o920@naver.com>
2024-11-08 19:35:58 +0900

    refactor: 예외 처리 클래스 사용

--- server/src/main/java/com/wap/wapor/controller/EmailController.java
+++ server/src/main/java/com/wap/wapor/controller/EmailController.java
@@ -2,9 +2,9 @@ package com.wap.wapor.controller;
 
 import com.wap.wapor.dto.EmailCheck;
 import com.wap.wapor.dto.EmailLoginRequest;
+import com.wap.wapor.exception.AuthCodeMismatchException;
 import com.wap.wapor.service.EmailSendService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,27 +17,15 @@ public class EmailController {
 
     // 이메일 인증 번호 전송
     @PostMapping("/mailSend")
-    public ResponseEntity<?> mailSend(@RequestBody @Valid EmailLoginRequest emailRequest) {
-        System.out.println("이메일 인증 요청이 들어옴");
-        System.out.println("이메일 인증 이메일:" + emailRequest.getIdentifier());
-
-        try {
-            String response = emailService.joinEmail(emailRequest.getIdentifier());
-            return ResponseEntity.ok("인증번호가 전송되었습니다: " + response); // 확인용
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("이메일 전송 중 오류가 발생했습니다.");
-        }
+    public ResponseEntity<String> mailSend(@RequestBody @Valid EmailLoginRequest emailRequest) {
+        emailService.joinEmail(emailRequest.getIdentifier());  // 인증 번호 전송
+        return ResponseEntity.ok("인증 번호가 전송되었습니다.");
     }
 
     // 인증 번호 확인
     @PostMapping("/mailAuthCheck")
     public ResponseEntity<String> authCheck(@RequestBody @Valid EmailCheck emailCheck) {
-        boolean isValid = emailService.verifyAuthCode(emailCheck.getAuthCode());
-
-        if (isValid) {
-            return ResponseEntity.ok("인증번호가 일치합니다.");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증번호가 일치하지 않습니다.");
-        }
+        emailService.verifyAuthCode(emailCheck.getAuthCode());
+        return ResponseEntity.ok("인증 번호가 일치합니다.");
     }
 }
