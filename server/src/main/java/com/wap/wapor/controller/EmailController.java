8fd03230190b83e88db952ea839ae1a32b6ea660
pu2rile <3o920@naver.com>
2024-11-12 17:50:47 +0900

    feature: redis를 사용한 이메일 인증 컨트롤러 추가

--- server/src/main/java/com/wap/wapor/controller/EmailController.java
+++ server/src/main/java/com/wap/wapor/controller/EmailController.java
@@ -1,8 +1,6 @@
 package com.wap.wapor.controller;
 
 import com.wap.wapor.dto.EmailCheck;
-import com.wap.wapor.dto.EmailLoginRequest;
-import com.wap.wapor.exception.AuthCodeMismatchException;
 import com.wap.wapor.dto.EmailSignUpRequest;
 import com.wap.wapor.service.EmailSendService;
 import lombok.RequiredArgsConstructor;
@@ -19,14 +17,14 @@ public class EmailController {
     // 이메일 인증 번호 전송
     @PostMapping("/mailSend")
     public ResponseEntity<String> mailSend(@RequestBody @Valid EmailSignUpRequest emailRequest) {
-        String authCode = emailService.joinEmail(emailRequest.getIdentifier());  // 인증 번호 전송
+        String authCode = emailService.requestAuthCode(emailRequest.getIdentifier());  // 인증 번호 전송
         return ResponseEntity.ok("인증 번호가 전송되었습니다. 인증 번호: " + authCode);
     }
 
     // 인증 번호 확인
     @PostMapping("/mailAuthCheck")
     public ResponseEntity<String> authCheck(@RequestBody @Valid EmailCheck emailCheck) {
-        emailService.verifyAuthCode(emailCheck.getAuthCode());
+        emailService.verifyAuthCode(emailCheck.getIdentifier(), emailCheck.getAuthCode());
         return ResponseEntity.ok("인증 번호가 일치합니다.");
     }
 }
