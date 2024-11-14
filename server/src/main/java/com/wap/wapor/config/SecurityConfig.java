2b10787fc11550441b9265c76176d0e516d7a450
dhlee777 <ukki5782@gmail.com>
2024-11-14 15:56:33 +0900

    feature:SecurityConfig추가

--- server/src/main/java/com/wap/wapor/config/SecurityConfig.java
+++ server/src/main/java/com/wap/wapor/config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package com.wap.wapor.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+                .authorizeHttpRequests(auth -> auth // Lambda 형태로 권한 설정
+                        .requestMatchers("/auth/kakao","/auth/kakao/callback").permitAll() // 인증 없이 접근 허용
+                        .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                );
+
+        return http.build();
+    }
 }
