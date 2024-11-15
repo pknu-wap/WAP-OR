c2232532f23a150c01a8e42c8e1bbd3cfcbcad35
dhlee777 <ukki5782@gmail.com>
2024-11-15 15:38:24 +0900

    refactor:SecurityConfig에 인증필터 추가
    user/me api 추가

--- server/src/main/java/com/wap/wapor/config/SecurityConfig.java
+++ server/src/main/java/com/wap/wapor/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.wap.wapor.config;
 
+import com.wap.wapor.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,8 +23,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth // Lambda 형태로 권한 설정
                         .requestMatchers("/auth/kakao","/auth/kakao/callback").permitAll() // 인증 없이 접근 허용
                         .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
-                );
-
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
--- server/src/main/java/com/wap/wapor/controller/KakaoAuthController.java
+++ server/src/main/java/com/wap/wapor/controller/KakaoAuthController.java
@@ -1,18 +1,25 @@
 package com.wap.wapor.controller;
 
 import com.wap.wapor.domain.User;
+import com.wap.wapor.domain.UserType;
 import com.wap.wapor.dto.AuthResponse;
+import com.wap.wapor.repository.UserRepository;
+import com.wap.wapor.security.UserPrincipal;
 import com.wap.wapor.service.KakaoAuthService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 public class KakaoAuthController {
     private final KakaoAuthService kakaoAuthService;
-
-    public KakaoAuthController(KakaoAuthService kakaoAuthService) {
+    private final UserRepository userRepository;
+    public KakaoAuthController(KakaoAuthService kakaoAuthService, UserRepository userRepository) {
         this.kakaoAuthService = kakaoAuthService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/kakao")
@@ -22,5 +29,13 @@ public class KakaoAuthController {
         AuthResponse authResponse = kakaoAuthService.processKakaoLogin(accessToken);
         return ResponseEntity.ok(authResponse);
     }
+    @GetMapping("/user/me")
+    public ResponseEntity<User> getCurrentUser(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        return userRepository
+                .findByIdentifierAndUserType(userPrincipal.getId(), UserType.KAKAO)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 
 }
