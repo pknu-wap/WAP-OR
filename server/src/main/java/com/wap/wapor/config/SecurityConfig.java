4b65b0a0598788deb6df4d677188931248e0a388
pu2rile <3o920@naver.com>
2024-11-15 14:23:46 +0900

    feature: 이메일 인증 엔드포인트 접근 허용 설정

---
+++ server/src/main/java/com/wap/wapor/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.wap.wapor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/mailSend", "/mailAuthCheck", "/api/users/register/email", "/api/users/login/email").permitAll() // 인증 없이 접근 가능 설정
+                        .anyRequest().authenticated() // 그 외의 요청은 인증 필요
+                );
+
+        return http.build();
+    }
+}
