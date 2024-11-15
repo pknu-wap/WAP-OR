86cd12ac142ffad922b34d306d8a623e3b920c1b
pu2rile <3o920@naver.com>
2024-11-15 14:24:30 +0900

    feature: 이메일 인증 관련 엔드포인트 구현
    RedisUtil을 통한 인증번호 임시 저장 기능 통합

--- server/src/main/java/com/wap/wapor/controller/EmailController.java
+++ server/src/main/java/com/wap/wapor/controller/EmailController.java
@@ -3,6 +3,7 @@ package com.wap.wapor.controller;
 import com.wap.wapor.dto.EmailCheck;
 import com.wap.wapor.dto.EmailSignUpRequest;
 import com.wap.wapor.service.EmailSendService;
+import com.wap.wapor.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import jakarta.validation.Valid;
 @RequiredArgsConstructor
 public class EmailController {
     private final EmailSendService emailService;
+    private final RedisUtil redisUtil;
 
     // 이메일 인증 번호 전송
     @PostMapping("/mailSend")
@@ -21,10 +23,11 @@ public class EmailController {
         return ResponseEntity.ok("인증 번호가 전송되었습니다. 인증 번호: " + authCode);
     }
 
-    // 인증 번호 확인
+    // 인증 번호 확인 및 인증 상태 저장
     @PostMapping("/mailAuthCheck")
     public ResponseEntity<String> authCheck(@RequestBody @Valid EmailCheck emailCheck) {
         emailService.verifyAuthCode(emailCheck.getIdentifier(), emailCheck.getAuthCode());
+        redisUtil.setDataExpire("verified:" + emailCheck.getIdentifier(), "true"); // 인증 성공 시 상태 저장 (TTL 설정 가능)
         return ResponseEntity.ok("인증 번호가 일치합니다.");
     }
 }
