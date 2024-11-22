b085ad919c0a23696f426c062e0be090f0354545
OSS-LDH <ukki5782@gmail.com>
2024-11-22 18:18:38 +0900

    feature: 페이로그 조회 컨트롤러 추가

--- server/src/main/java/com/wap/wapor/controller/PayLogController.java
+++ server/src/main/java/com/wap/wapor/controller/PayLogController.java
@@ -1,8 +1,13 @@
 package com.wap.wapor.controller;
 
+import com.wap.wapor.dto.GetPayLogDto;
 import com.wap.wapor.dto.PostPayLogDto;
 import com.wap.wapor.security.UserPrincipal;
 import com.wap.wapor.service.PayLogService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +26,14 @@ public class PayLogController {
        Long postId= payLogService.payLogPost(postPayLogDto, userPrincipal);
        return ResponseEntity.ok(postId);
 
+    }
+    @GetMapping("/search")
+    public Page<GetPayLogDto> getPublicPayLogs(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "6") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+        return payLogService.getPublicPayLogs(pageable);
     }
 
 }
