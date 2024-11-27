fa048fa9bdddb867efd9d20445782eda5a5010b5
pu2rile <3o920@naver.com>
2024-11-27 13:41:27 +0900

    feature: 사용자 기반 가상계좌 조회 메서드 추가

--- server/src/main/java/com/wap/wapor/repository/VirtualAccountRepository.java
+++ server/src/main/java/com/wap/wapor/repository/VirtualAccountRepository.java
@@ -1,7 +1,12 @@
 package com.wap.wapor.repository;
 
+import com.wap.wapor.domain.User;
 import com.wap.wapor.domain.VirtualAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface VirtualAccountRepository extends JpaRepository<VirtualAccount, Long> {
+    Optional<VirtualAccount> findByUser(User user);
 }
