20c6d98b4cec39c5850d35ef55b0dc744a7d04ba
pu2rile <3o920@naver.com>
2024-11-27 16:34:04 +0900

    fix: 반환값 수정
    입금 금액만 요청하도록

--- server/src/main/java/com/wap/wapor/dto/DepositRequest.java
+++ server/src/main/java/com/wap/wapor/dto/DepositRequest.java
@@ -7,6 +7,4 @@ import lombok.Data;
 public class DepositRequest {
     @Min(value = 1, message = "입금 금액은 0보다 커야 합니다.")
     private Long amount;
-    private Long accountId;
-    private String identifier;
 }
