876bb333771846052f9d17c35dcf8d533d4ad904
pu2rile <3o920@naver.com>
2024-11-27 13:38:43 +0900

    feature: 입금 최솟값 설정

--- server/src/main/java/com/wap/wapor/dto/DepositRequest.java
+++ server/src/main/java/com/wap/wapor/dto/DepositRequest.java
@@ -1,9 +1,11 @@
 package com.wap.wapor.dto;
 
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class DepositRequest {
+    @Min(value = 1, message = "입금 금액은 0보다 커야 합니다.")
     private Long amount;
     private Long accountId;
     private String identifier;
