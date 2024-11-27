3a3ecd26c917466f3033675f30cebccdb7ea15e8
pu2rile <3o920@naver.com>
2024-11-27 13:36:30 +0900

    feature: 유저 타입에 이메일 추가해서 수정

--- server/src/main/java/com/wap/wapor/repository/UserRepository.java
+++ server/src/main/java/com/wap/wapor/repository/UserRepository.java
@@ -3,11 +3,20 @@ package com.wap.wapor.repository;
 import com.wap.wapor.domain.User;
 import com.wap.wapor.domain.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    // identifier로 사용자 조회
     Optional<User> findByIdentifier(String identifier);
-    Optional<User> findByIdentifierAndUserType(String identifier, UserType usertype);
-    // identifier 중복 여부 확인 메서드
+
+    // identifier와 userType으로 사용자 조회
+    Optional<User> findByIdentifierAndUserType(String identifier, UserType userType);
+
+    // identifier 중복 여부 확인
     boolean existsByIdentifier(String identifier);
+
+    // identifier와 여러 userType으로 사용자 조회
+    Optional<User> findByIdentifierAndUserTypeIn(String identifier, List<UserType> userTypes);
 }
