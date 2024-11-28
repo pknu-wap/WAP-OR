997bc05f364a168de0ac77f5dde7918d2894a18e
OSS-LDH <ukki5782@gmail.com>
2024-11-28 23:16:04 +0900

    refactor: 페이로그등록 컨트롤러 수정
    매개변수 수정

--- server/src/main/java/com/wap/wapor/controller/PayLogController.java
+++ server/src/main/java/com/wap/wapor/controller/PayLogController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -27,7 +28,7 @@ public class PayLogController {
     }
 
     @PostMapping
-    public ResponseEntity<PayLogResponse> createPayLog(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestBody PostPayLogDto postPayLogDto) {
+    public ResponseEntity<PayLogResponse> createPayLog(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestPart("images") List<MultipartFile> imageFile,@RequestPart("paylog")PostPayLogDto postPayLogDto) {
         PayLogResponse response = payLogService.createPayLog(postPayLogDto, userPrincipal);
         return ResponseEntity.ok(response);
     }
